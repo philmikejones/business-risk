6688ed629ad1246ff52bb7861ce9c2bbd042dca3
Phil Mike Jones <philmikejones@gmail.com>
2020-10-09 22:36:06 +0100

    plot cities with size and colour

--- data-raw/plot-business-risk.R
+++ data-raw/plot-business-risk.R
@@ -1,9 +1,11 @@
+library("assertthat")
 library("dplyr")
 library("readr")
 library("stringr")
 library("sf")
 library("rmapshaper")
 library("ggplot2")
+library("viridis")
 
 if(!file.exists("inst/extdata/region/infuse_rgn_2011.shp")) {
     download.file("https://borders.ukdataservice.ac.uk/ukborders/easy_download/prebuilt/shape/infuse_rgn_2011.zip", destfile = "inst/extdata/region/infuse_rgn_2011.zip")
@@ -27,13 +29,31 @@ region =
 region = bind_rows(region, country)
 region = rmapshaper::ms_simplify(region, keep = 0.02)
 
-risk = readr::read_csv("cities-business-risk.csv")
-risk
-
 cities = 
     readr::read_csv("cities-coordinates.csv", skip = 1) %>%  # first row is credit
     sf::st_as_sf(coords = c("longitude", "latitude")) %>%
     sf::st_set_crs(4326) %>%
     sf::st_transform(crs = sf::st_crs(27700))
 
-cities
+risk = 
+    readr::read_csv("cities-business-risk.csv") %>%
+    mutate(
+        City = if_else(City == "Kingston upon Hull", "Hull", City),
+        City = if_else(City == "Newcastle upon Tyne", "Newcastle Upon Tyne", City),
+        City = if_else(City == "Brighton", "Brighton & Hove", City)
+    )
+
+nrow_risk = nrow(risk)
+
+risk = left_join(risk, cities, by = c("City" = "city"))
+risk = sf::st_as_sf(risk)
+
+stopifnot(
+    all.equal(nrow(risk), nrow_risk),
+    all(!is.na(unlist(risk$geometry)))
+)
+
+ggplot() +
+    geom_sf(data = region) +
+    geom_sf(data = risk, aes(size = `% of total population`, colour = `% of total population`)) +
+    scale_colour_viridis()
