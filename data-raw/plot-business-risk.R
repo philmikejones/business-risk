bf27629855b78c48513d1030171247d0329d2217
Phil Mike Jones <philmikejones@gmail.com>
2020-10-10 20:47:21 +0100

    plot individual regions

--- data-raw/plot-business-risk.R
+++ data-raw/plot-business-risk.R
@@ -7,27 +7,27 @@ library("rmapshaper")
 library("ggplot2")
 library("viridis")
 
-if(!file.exists("inst/extdata/region/infuse_rgn_2011.shp")) {
-    download.file("https://borders.ukdataservice.ac.uk/ukborders/easy_download/prebuilt/shape/infuse_rgn_2011.zip", destfile = "inst/extdata/region/infuse_rgn_2011.zip")
-    unzip("inst/extdata/region/infuse_rgn_2011.zip", exdir = "inst/extdata/region")
+if(!file.exists("inst/extdata/regions/infuse_rgn_2011.shp")) {
+    download.file("https://borders.ukdataservice.ac.uk/ukborders/easy_download/prebuilt/shape/infuse_rgn_2011.zip", destfile = "inst/extdata/regions/infuse_rgn_2011.zip")
+    unzip("inst/extdata/regions/infuse_rgn_2011.zip", exdir = "inst/extdata/regions")
 }
 
-if(!file.exists("inst/extdata/region/infuse_ctry_2011.shp")){
-    download.file("https://borders.ukdataservice.ac.uk/ukborders/easy_download/prebuilt/shape/infuse_ctry_2011.zip", destfile = "inst/extdata/region/infuse_ctry_2011.zip")
-    unzip("inst/extdata/region/infuse_ctry_2011.zip", exdir = "inst/extdata/region")
+if(!file.exists("inst/extdata/regions/infuse_ctry_2011.shp")){
+    download.file("https://borders.ukdataservice.ac.uk/ukborders/easy_download/prebuilt/shape/infuse_ctry_2011.zip", destfile = "inst/extdata/regions/infuse_ctry_2011.zip")
+    unzip("inst/extdata/regions/infuse_ctry_2011.zip", exdir = "inst/extdata/regions")
 }
 
 country = 
-    sf::read_sf("inst/extdata/region/infuse_ctry_2011.shp") %>%
+    sf::read_sf("inst/extdata/regions/infuse_ctry_2011.shp") %>%
     filter(geo_label == "Scotland" | geo_label == "Wales") %>%
     select(c("geo_code", "geo_label", "label", "name", "geometry"))
 
-region =
-    sf::read_sf("inst/extdata/region/infuse_rgn_2011.shp") %>%
+regions =
+    sf::read_sf("inst/extdata/regions/infuse_rgn_2011.shp") %>%
     select(c("geo_code", "geo_label", "label", "name", "geometry"))
 
-region = bind_rows(region, country)
-region = rmapshaper::ms_simplify(region, keep = 0.02)
+regions = bind_rows(regions, country)
+regions = rmapshaper::ms_simplify(regions, keep = 0.02)
 
 cities = 
     readr::read_csv("cities-coordinates.csv", skip = 1) %>%  # first row is credit
@@ -53,7 +53,11 @@ stopifnot(
     all(!is.na(unlist(risk$geometry)))
 )
 
-ggplot() +
-    geom_sf(data = region) +
-    geom_sf(data = risk, aes(size = `% of total population`, colour = `% of total population`)) +
-    scale_colour_viridis()
+for(i in seq_along(nrow(regions))) {
+    region = regions[i, ]
+    plot = ggplot() +
+        geom_sf(data = region) +
+        geom_sf(data = risk, aes(size = `% of total population`, colour = `% of total population`)) +
+        scale_colour_viridis()
+    ggsave(plot, file = paste0("region_", i, ".pdf"))
+}
